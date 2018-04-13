5aa8055db8fe8eda6c8f7cfa3ac6a5da565252cb
David Lee <davlee1972@yahoo.com>
2018-04-13 11:59:08 -0700

    Update convert_xml_to_json.py

--- xml_to_json/convert_xml_to_json.py
+++ xml_to_json/convert_xml_to_json.py
@@ -12,6 +12,11 @@ import subprocess
 import os
 import gzip
 import logging
+import shutil
+import sys
+
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.DEBUG)
 
 
 def decimal_default(obj):
@@ -43,7 +48,7 @@ class ParqConverter(xmlschema.XMLSchemaConverter):
         CDATA parts are ignored if this argument is `None`.
         """
 
-        super(ParqConverter, self).__init__(
+        super().__init__(
             namespaces, dict_class or OrderedDict, list_class,
             attr_prefix=None, text_key=None, cdata_prefix=None
         )
@@ -69,7 +74,7 @@ class ParqConverter(xmlschema.XMLSchemaConverter):
                 if value:
                     if xsd_child.is_single():
                         if xsd_child.type.is_simple() or xsd_child.type.has_simple_content():
-                            for k in value.keys():
+                            for k in value:
                                 result_dict[k] = value[k]
                         else:
                             result_dict[name] = value
@@ -102,58 +107,25 @@ def open_file(zip, filename):
         return open(filename, "wb")
 
 
-def parse_file(xml_file, output_format, zip, xsd_file, xpath, no_overwrite, target_path):
+def parse_file(xml_file, output_file, xsd_file, output_format, zip, xpath):
     """
-    :param xml_file: xml file name
+    :param xml_file: xml file
+    :param output_file: output file
+    :param xsd_file: xsd file
     :param output_format: jsonl or json
     :param zip: zip save file
-    :param xsd_file: xsd file name
     :param xpath: whether to parse a specific xml path
-    :param no_overwrite: overwrite target file
-    :param target_path: directory to save file
     """
 
-    logging.debug("Generating schema from " + xsd_file)
+    _logger.debug("Generating schema from " + xsd_file)
 
     my_schema = xmlschema.XMLSchema(xsd_file, converter=ParqConverter)
 
-    path, xml_file = os.path.split(os.path.realpath(xml_file))
+    _logger.debug("Parsing " + xml_file)
 
-    if not target_path:
-        tgt_path = path
-    elif target_path[:5] == "hdfs:":
-        tgt_path = path
-    else:
-        tgt_path = target_path
+    _logger.debug("Writing to file " + output_file)
 
-    logging.debug("Parsing " + xml_file)
-
-    if xml_file[-4:] == ".xml":
-        if output_format == "jsonl":
-            output_file = xml_file[:-4] + ".jsonl"
-        else:
-            output_file = xml_file[:-4] + ".json"
-    else:
-        if output_format == "jsonl":
-            output_file = xml_file + ".jsonl"
-        else:
-            output_file = xml_file + ".json"
-
-    if zip:
-        output_file = output_file + ".gz"
-
-    if no_overwrite:
-        if target_path and target_path[:5] == "hdfs:":
-            if subprocess.call(["hadoop", "fs", "-test", "-e", target_path]) == 0:
-                logging.debug("No overwrite. Skipping " + xml_file)
-                return
-        elif os.path.isfile(tgt_path + "/" + output_file):
-            logging.debug("No overwrite. Skipping " + xml_file)
-            return
-
-    logging.debug("Writing to file " + output_file)
-
-    context = ET.iterparse(path + "/" + xml_file, events=("start", "end"))
+    context = ET.iterparse(xml_file, events=("start", "end"))
 
     xpath_list = None
 
@@ -180,10 +152,10 @@ def parse_file(xml_file, output_format, zip, xsd_file, xpath, no_overwrite, targ
         if xsd_elem.max_occurs is None or xsd_elem.max_occurs > 1:
             isjsonarray = True
 
-        with open_file(zip, tgt_path + "/" + output_file) as json_file:
+        with open_file(zip, output_file) as json_file:
 
             # 2nd pass open xml file and get elements in the list
-            logging.debug("Parsing " + xpath_list[-1] + " from " + xml_file)
+            _logger.debug("Parsing " + xpath_list[-1] + " from " + xml_file)
 
             if isjsonarray and output_format != "jsonl":
                 json_file.write(bytes("[\n", "utf-8"))
@@ -211,8 +183,8 @@ def parse_file(xml_file, output_format, zip, xsd_file, xpath, no_overwrite, targ
                                 json_file.write(bytes(",\n" + my_json, "utf-8"))
 
                         # my_json = json.dumps(my_dict, default=decimal_default)
-                        # logging.info(my_json)
-                        # logging.info("\n")
+                        # _logger.info(my_json)
+                        # _logger.info("\n")
                         # input("Press Enter to continue...")
 
                     if not elem_active:
@@ -223,79 +195,134 @@ def parse_file(xml_file, output_format, zip, xsd_file, xpath, no_overwrite, targ
             if isjsonarray and output_format != "jsonl":
                 json_file.write(bytes("\n]", "utf-8"))
 
+        # Remove file if no json is generated
         if first_record:
-            os.remove(tgt_path + "/" + output_file)
+            os.remove(output_file)
             return
 
     else:
         elem, root = next(context)
-        my_dict = my_schema.to_dict(path + "/" + xml_file)
+        my_dict = my_schema.to_dict(xml_file)
         my_json = '{"' + root.tag + '": ' + json.dumps(my_dict, default=decimal_default) + "}"
-        with open_file(zip, tgt_path + "/" + output_file) as json_file:
+        with open_file(zip, output_file) as json_file:
             json_file.write(bytes(my_json, "utf-8"))
 
     del context
 
-    if target_path and target_path[:5] == "hdfs:":
-        subprocess.call(["hadoop", "fs", "-mkdir", "-p", target_path])
-        subprocess.call(["hadoop", "fs", "-rm", "-f", target_path + "/" + output_file])
-        subprocess.call(["hadoop", "fs", "-put", tgt_path + "/" + output_file, target_path])
-        os.remove(tgt_path + "/" + output_file)
-
-    logging.debug("Completed " + xml_file)
+    _logger.debug("Completed " + xml_file)
 
 
-def convert_xml_to_json(args):
+def convert_xml_to_json(xsd_file=None, output_format="jsonl", server=None, target_path=None, zip=False, xpath=None, multi=1, no_overwrite=False, verbose="DEBUG", log=None, xml_files=None):
     """
-    :param args: args passed in from command line
+    :param xsd_file: xsd file name
+    :param output_format: jsonl or json
+    :param server: optional server with hadoop client installed if current server does not have hadoop installed
+    :param target_path: directory to save file
+    :param zip: zip save file
+    :param xpath: whether to parse a specific xml path
+    :param multi: how many files to convert concurrently
+    :param no_overwrite: overwrite target file
+    :param verbose: stdout log messaging level
+    :param log: optional log file
+    :param xml_files: list of xml_files
+
     """
 
-    _logger = logging.getLogger()
-    _logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter("%(levelname)s - %(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
 
-    # create console handler and set level to info
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    handler.setLevel(logging.getLevelName(args['verbose']))
-    _logger.addHandler(handler)
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    ch.setLevel(logging.getLevelName(verbose))
+    _logger.addHandler(ch)
 
-    if args['log']:
+    if log:
         # create log file handler and set level to debug
-        handler = logging.FileHandler(args['log'])
-        handler.setFormatter(formatter)
-        handler.setLevel(logging.DEBUG)
-        _logger.addHandler(handler)
+        fh = logging.FileHandler(log)
+        fh.setFormatter(formatter)
+        fh.setLevel(logging.DEBUG)
+        _logger.addHandler(fh)
 
-    logging.info("Parsing XML Files..")
+    _logger.info("Parsing XML Files..")
 
-    if args['target_path']:
-        if args['target_path'][:1] == "/" and not os.path.exists(args['target_path']):
-            os.makedirs(args['target_path'])
-        elif args['target_path'][:5] == "hdfs:":
-            if subprocess.call(["hadoop", "fs", "-test", "-e", args['target_path']]) != 0:
-                logging.error("--target_path not valid or hadoop client not installed on server")
-                return
+    if target_path:
+        if target_path.startswith("hdfs:"):
+            if server:
+                if subprocess.call(["ssh", server, "hadoop fs -test -e " + target_path]) != 0:
+                    _logger.error("invalid target_path: " + target_path + " using hadoop server: " + server)
+                    sys.exit(1)
+            elif shutil.which("hadoop"):
+                if subprocess.call(["hadoop", "fs", "-test", "-e", target_path]) != 0:
+                    _logger.error("invalid target_path: " + target_path)
+                    sys.exit(1)
+            else:
+                _logger.error("no hadoop client found")
+                sys.exit(1)
         else:
-            logging.error("invalid --target_path specified")
-            return
+            if not os.path.exists(target_path):
+                _logger.error("invalid target_path specified")
+                sys.exit(1)
 
     # open target files
-    file_list = list(set([f for _files in [glob.glob(args['args'][x]) for x in range(0, len(args['args']))] for f in _files]))
+    file_list = list(set([f for _files in [glob.glob(xml_files[x]) for x in range(0, len(xml_files))] for f in _files]))
     file_count = len(file_list)
 
-    parse_queue_pool = Pool(processes=int(args['multi']))
+    parse_queue_pool = Pool(processes=multi)
 
-    logging.info("Processing " + str(file_count) + " files")
+    _logger.info("Processing " + str(file_count) + " files")
 
     if len(file_list) <= 1000:
         file_list.sort(key=os.path.getsize, reverse=True)
-        logging.info("Parsing files in the following order:")
-        logging.info(file_list)
+        _logger.info("Parsing files in the following order:")
+        _logger.info(file_list)
 
     for filename in file_list:
-        parse_queue_pool.apply_async(parse_file, args=(filename, args['output_format'], args['zip'], args['xsd_file'], args['xpath'], args['no_overwrite'], args['target_path']), error_callback=logging.info)
-        # parse_file(filename, args["output_format"], args["zip"], args["xsd_file"], args["xpath"], args["no_overwrite"], args['target_path'])
+
+        path, xml_file = os.path.split(os.path.realpath(filename))
+
+        if xml_file.endswith(".xml"):
+            if output_format == "jsonl":
+                output_file = xml_file[:-4] + ".jsonl"
+            else:
+                output_file = xml_file[:-4] + ".json"
+        else:
+            if output_format == "jsonl":
+                output_file = xml_file + ".jsonl"
+            else:
+                output_file = xml_file + ".json"
+
+        if zip:
+            output_file = output_file + ".gz"
+
+        if not target_path:
+            output_file = os.path.join(path, output_file)
+            if no_overwrite and os.path.isfile(output_file):
+                _logger.debug("No overwrite. Skipping " + xml_file)
+                continue
+        elif target_path.startswith("hdfs:"):
+            if no_overwrite and subprocess.call(["hadoop", "fs", "-test", "-e", os.path.join(target_path, output_file)]) == 0:
+                _logger.debug("No overwrite. Skipping " + xml_file)
+                continue
+            output_file = os.path.join(path, output_file)
+        else:
+            output_file = os.path.join(target_path, output_file)
+            if no_overwrite and os.path.isfile(output_file):
+                _logger.debug("No overwrite. Skipping " + xml_file)
+                continue
+
+        parse_queue_pool.apply_async(parse_file, args=(filename, output_file, xsd_file, output_format, zip, xpath), error_callback=_logger.info)
+        # parse_file(parse_file(xml_file, output_file, xsd_file, output_format, zip, xpath)
+
+        if target_path and target_path.startswith("hdfs:") and os.path.isfile(output_file):
+            if server:
+                if subprocess.call(["ssh", server, "hadoop fs -put -f " + output_file + " " + target_path]) != 0:
+                    _logger.error("invalid target_path specified")
+                    sys.exit(1)
+            else:
+                if subprocess.call(["hadoop", "fs", "-put", "-f", output_file, target_path]) != 0:
+                    _logger.error("invalid target_path specified")
+                    sys.exit(1)
+
+            os.remove(output_file)
 
     parse_queue_pool.close()
     parse_queue_pool.join()
