6d55c7897f52da9e6b0ef48c08aff21177668c3f
David Lee <davlee1972@yahoo.com>
2018-04-13 11:58:10 -0700

    Update test_convert_xml_to_json.py

--- test/test_convert_xml_to_json.py
+++ test/test_convert_xml_to_json.py
@@ -1,6 +1,7 @@
 import unittest
 import json
 import os
+import tempfile
 
 from xml_to_json.convert_xml_to_json import parse_file
 
@@ -12,19 +13,18 @@ class MyTest(unittest.TestCase):
         realpath = os.path.dirname(os.path.realpath(__file__))
 
         xml_file = os.path.join(realpath, "PurchaseOrder.xml")
+        output_file = os.path.join(tempfile.gettempdir(), "PurchaseOrder.json")
+        xsd_file = os.path.join(realpath, "PurchaseOrder.xsd")
         output_format = "json"
         zip = False
-        xsd_file = os.path.join(realpath, "PurchaseOrder.xsd")
         xpath = None
-        no_overwrite = False
-        target_path = "/tmp"
 
-        parse_file(xml_file, output_format, zip, xsd_file, xpath, no_overwrite, target_path)
+        parse_file(xml_file, output_file, xsd_file, output_format, zip, xpath)
         with open(os.path.join(realpath,"PurchaseOrder.json")) as f:
             test_json = json.loads(f.read())
-        with open("/tmp/PurchaseOrder.json") as f:
+        with open(output_file) as f:
             target_json = json.loads(f.read())
-        os.remove("/tmp/PurchaseOrder.json")
+        os.remove(output_file)
         print("Original")
         print("=================================")
         print(test_json)
@@ -38,24 +38,23 @@ class MyTest(unittest.TestCase):
         realpath = os.path.dirname(os.path.realpath(__file__))
 
         xml_file = os.path.join(realpath, "PurchaseOrder.xml")
+        output_file = os.path.join(tempfile.gettempdir(), "PurchaseOrder.jsonl")
+        xsd_file = os.path.join(realpath, "PurchaseOrder.xsd")
         output_format = "jsonl"
         zip = False
-        xsd_file = os.path.join(realpath, "PurchaseOrder.xsd")
         xpath = "/purchaseOrder/items/item"
-        no_overwrite = False
-        target_path = "/tmp"
 
         test_json = list()
         target_json = list()
 
-        parse_file(xml_file, output_format, zip, xsd_file, xpath, no_overwrite, target_path)
-        with open(os.path.join(realpath,"PurchaseOrder.jsonl")) as f:
+        parse_file(xml_file, output_file, xsd_file, output_format, zip, xpath)
+        with open(os.path.join(realpath, "PurchaseOrder.jsonl")) as f:
             for line in f:
                 test_json.append(json.loads(line))
-        with open("/tmp/PurchaseOrder.jsonl") as f:
+        with open(output_file) as f:
             for line in f:
                 target_json.append(json.loads(line))
-        os.remove("/tmp/PurchaseOrder.jsonl")
+        os.remove(output_file)
         print("Original")
         print("=================================")
         print(test_json)
